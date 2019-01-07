0583a6ad64e90cd527fb404d3d5b6e0fdc548f73
Karan Kharecha <kharechakaran67@gmail.com>
2019-01-07 17:07:11 +0530

    changes in binning function

--- src/binning.py
+++ src/binning.py
@@ -1,26 +1,28 @@
 import numpy as np
 import copy
 
-bins = dict()
 
+def by_means(data, bin_size):
+    bins = dict()
+    data = np.array(data)
+    data = np.sort(data)
 
-def by_means():
+    split_array = np.split(data, range(0, len(data), bin_size))
+
+    for idx in range(1, len(split_array)):
+        bins[idx] = split_array[idx]
+
     bins_mean = copy.deepcopy(bins)
-    for bin in bins_mean.values():
-        mean = np.floor(np.mean(bin))
-        bin.fill(mean)
+
+    for sub_bin in bins_mean.values():
+        mean = np.floor(np.mean(sub_bin))
+        sub_bin.fill(mean)
+        
     return bins_mean
 
 
 if __name__ == '__main__':
-    bin_size = 4
-    data = [37, 30, 28, 13, 7, 3, 22, 8, 22, 22, 26, 26]
-    data = np.array(data)
-    data = np.sort(data)
-    split_array = np.split(data, range(0, len(data), bin_size))
-    for idx in range(1, len(split_array)):
-        bins[idx] = split_array[idx]
+    size = 4
+    list_sequence = [37, 30, 28, 13, 7, 3, 22, 8, 22, 22, 26, 26]
 
-    print("original: ", bins)
-
-    print("binning: ", by_means())
+    print("binning: ", by_means(data=list_sequence, bin_size=size))
