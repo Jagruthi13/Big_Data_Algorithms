f7d538140f8763ff95c8cd8e767c28e1f3cb03d9
Karan Kharecha <kharechakaran67@gmail.com>
2019-01-08 15:16:33 +0530

    code optimization and binning by median added

--- src/apriori.py
+++ src/apriori.py
@@ -2,35 +2,30 @@ from itertools import chain, combinations
 
 support_count_dict = dict()
 subsets = dict()
-items_dict = dict()
+sup_items_dict = dict()
 
 
 def generate_subsets():
-    for subset in chain(*map(lambda x: combinations(items_dict.keys(), x), range(1, len(items_dict.keys()) + 1))):
+    for subset in chain(*map(lambda x: combinations(sup_items_dict.keys(), x), range(1, len(sup_items_dict.keys()) + 1))):
         if len(subset) not in subsets.keys():
             subsets[len(subset)] = set()
         (subsets[len(subset)]).add(subset)
 
 
 def perform_apriori(data, support_count):
-    sup_dict = dict()
+    items_dict = dict()
     for value in data.values():
         for item in value:
             if item in items_dict.keys():
                 items_dict[item] += 1
                 if items_dict[item] >= support_count:
-                    key = "(" + item + ")"
-                    if key in sup_dict.keys():
-                        sup_dict[key] += 1
-                    else:
-                        sup_dict[key] = items_dict[item]
+                    sup_items_dict[item] = items_dict[item]
             else:
                 items_dict[item] = 1
-    support_count_dict[1] = sup_dict
 
     generate_subsets()
 
-    for total_items in range(2, len(subsets) + 1):
+    for total_items in range(1, len(subsets) + 1):
         for s in subsets[total_items]:
             scount = 0
             for items in data.values():
@@ -42,16 +37,16 @@ def perform_apriori(data, support_count):
                 key = "(" + ", ".join(s) + ")"
                 if str(s) in support_count_dict[total_items].keys():
                     (support_count_dict[total_items])[key].add({key: scount})
-                    print()
                 else:
                     (support_count_dict[total_items])[key] = scount
 
     for k, v in support_count_dict.items():
         print(k, v)
+    return support_count_dict
 
 
 if __name__ == '__main__':
-    sup_count = 2
+    sup_count = 3
     table = {
         't1': {'i1', 'i2', 'i5'},
         't2': {'i2', 'i4'},
--- src/binning.py
+++ src/binning.py
@@ -15,14 +15,21 @@ def create_bins(data, bin_size):
     return copy.deepcopy(bins)
 
 
-def by_means(data, bin_size):
-    bins_mean = create_bins(data, bin_size)
+def by_ms(data, bin_size, type):
+    bins_ms = create_bins(data, bin_size)
 
-    for sub_bin in bins_mean.values():
-        mean = np.floor(np.mean(sub_bin))
-        sub_bin.fill(mean)
+    if type is "mean":
+        for sub_bin in bins_ms.values():
+            mean = np.floor(np.mean(sub_bin))
+            sub_bin.fill(mean)
+    elif type is "median":
+        for sub_bin in bins_ms.values():
+            mean = np.floor(np.median(sub_bin))
+            sub_bin.fill(mean)
+    else:
+        return bins_ms
 
-    return bins_mean
+    return bins_ms
 
 
 def by_boundaries(data, bin_size):
@@ -41,5 +48,5 @@ if __name__ == '__main__':
     size = 4
     list_sequence = [37, 30, 28, 13, 7, 3, 22, 8, 22, 22, 26, 26]
 
-    print("binning: ", by_means(data=list_sequence, bin_size=size))
+    print("binning: ", by_ms(data=list_sequence, bin_size=size, type="mean"))
     print("binning: ", by_boundaries(data=list_sequence, bin_size=size))
